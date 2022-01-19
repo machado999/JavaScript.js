8e5f1a0ebbfa925c055466997a3cdae0e2e258b8
Gabriell Nunes <83989470+machado999@users.noreply.github.com>
2022-01-19 18:52:50 -0300

    Add files via upload

---
+++ if_tenario/script.js
@@ -0,0 +1,8 @@
+//if tenario
+
+var nome = prompt('Digite seu nome: ');
+var idade = prompt('Digite sua idade: ');
+var salario = prompt('Digite seu sálario: ');
+
+salario >= 1500 && idade >=18 ? console.log('Parabéns seu crédito foi pré aprovado!!') : 
+console.log('Infelizmente seu crédito não foi aprovado');
